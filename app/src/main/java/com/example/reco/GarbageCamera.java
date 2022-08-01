e536d8a645b1a5c5e7b412412fbd118d3b6daf79
KMJ <alswn5790@naver.com>
2022-08-01 20:03:08 +0900

    2차

--- app/src/main/java/com/example/reco/GarbageCamera.java
+++ app/src/main/java/com/example/reco/GarbageCamera.java
@@ -1,135 +1,179 @@
 package com.example.reco;
 
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.ActivityCompat;
-import androidx.core.content.ContextCompat;
 
-import android.Manifest;
-import android.content.ClipData;
-import android.content.ClipboardManager;
-import android.content.Context;
-import android.content.Intent;
-import android.content.pm.PackageManager;
+import android.content.DialogInterface;
+import android.content.res.AssetManager;
 import android.graphics.Bitmap;
-import android.net.Uri;
+import android.graphics.drawable.BitmapDrawable;
 import android.os.Bundle;
-import android.provider.MediaStore;
-import android.util.SparseArray;
-import android.view.View;
-import android.widget.Button;
+import android.os.PersistableBundle;
+import android.util.Log;
 import android.widget.ImageView;
-import android.widget.TextView;
-import android.widget.Toast;
 
-import com.google.android.gms.vision.Frame;
-import com.google.android.gms.vision.text.TextBlock;
-import com.google.android.gms.vision.text.TextRecognizer;
+import com.googlecode.tesseract.android.TessBaseAPI;
 
-import java.io.IOException;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.io.OutputStream;
 
 public class GarbageCamera extends AppCompatActivity {
 
-    Button button_capture, button_copy;
-    TextView textview_data;
-    Bitmap bitmap;
-    private static final int REQUEST_CAMERA_CODE = 100;
-    private ImageView imageView;
-
+    //TODO [로직 [3] : 필요한 전역 변수 선언 실시]
+    TessBaseAPI tessBaseAPI;
+    String dataPath = "";
+    String langData = "kor+eng";
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(@Nullable Bundle savedInstanceState, @Nullable PersistableBundle persistentState) {
+        //TODO [로직 [4] : oncreate 부분에서 TessBaseAPI 객체 초기화 수행 실시]
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_garbage_camera);
 
-        button_capture = findViewById(R.id.button_capture);
-        button_copy = findViewById(R.id.button_copy);
-        textview_data = findViewById(R.id.text_data);
+        try {
+            dataPath = getFilesDir() + "/tesseract/";
+            checkFile(new File(dataPath + "tessdata/"), "kor");
+            checkFile(new File(dataPath + "tessdata/"), "eng");
 
-        if (ContextCompat.checkSelfPermission(GarbageCamera.this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
-            ActivityCompat.requestPermissions(GarbageCamera.this, new String[]{
-                    Manifest.permission.CAMERA
-            }, REQUEST_CAMERA_CODE);
+            tessBaseAPI = new TessBaseAPI();
+            tessBaseAPI.init(dataPath, langData);
         }
-
-        button_capture.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                CropImage.activity().setGuidelines(CropImageView.Guidelines.ON).start(GarbageCamera.this);
-            }
-        });
-
-        button_copy.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                String scanned_text = textview_data.getText().toString();
-                copyToClipBoard(scanned_text);
-            }
-        });
-
+        catch(Exception e){
+            e.printStackTrace();
+        }
     }
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        if (requestCode == CropImage.CROP_IMAGE_ACTIVITY_REQEUST_CODE){
-            CropImage.ActivityResult result = CropImage.getActivityResult(data);
-            if (requestCode == RESULT_OK){
-                Uri resultUri = result.getUri();
-                try {
-                    bitmap = MediaStore.Images.Media.getBitmap(this.getContentResolver(), resultUri);
-                    getTextFromImage(bitmap);
 
-                } catch (IOException e) {
-                    e.printStackTrace();
+
+    //TODO [로직 [5] : OCR 인식 위한 파일 존재 확인 메소드 작성]
+    public void checkFile(File dir, String lang){
+        Log.d("---","---");
+        Log.d("//===========//","================================================");
+        Log.d("","\n"+"[A_CameraOcr > checkFile() 메소드 : OCR 인식 위한 파일 존재 확인 실시]");
+        Log.d("","\n"+"[언어 파일 : "+String.valueOf(lang)+"]");
+        Log.d("//===========//","================================================");
+        Log.d("---","---");
+        try {
+            if(!dir.exists() && dir.mkdirs()){
+                copyFiles(lang);
+            }
+
+            if(dir.exists()){
+                String dataFilePath = dataPath + "/tessdata/" + lang + ".traineddata";
+                File dataFile = new File(dataFilePath);
+                if(!dataFile.exists()){
+                    copyFiles(lang);
                 }
             }
         }
-
+        catch (Exception e){
+            e.printStackTrace();
+        }
     }
 
-    // 사진 찍기
-//    public void takePicture(){
-//        Intent imageTakeIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-//
-//        if (imageTakeIntent.resolveActivity(getPackageManager()) != null){
-//            startActivityForResult(imageTakeIntent, REQUEST_CAMERA_CODE);
-//        }
-//    }
 
-    // 결과값 가져오기
-    @Override
-//    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-//        super.onActivityResult(requestCode, resultCode, data);
-//        if (requestCode == REQUEST_CAMERA_CODE && resultCode == RESULT_OK) {
-//            Bundle extras = data.getExtras();
-//            Uri resultUri = result.getUri();
-//            bitmap = MediaStore.Images.Media.getBitmap(this.getContentResolver(), resultUri);
-//        }
-//    }
+    //TODO [로직 [6] : OCR 인식 위한 언어 파일 복사 메소드 작성]
+    public void copyFiles(String lang){
+        Log.d("---","---");
+        Log.d("//===========//","================================================");
+        Log.d("","\n"+"[A_CameraOcr > copyFiles() 메소드 : OCR 인식 위한 언어 파일 복사 실시]");
+        Log.d("","\n"+"[언어 파일 : "+String.valueOf(lang)+"]");
+        Log.d("//===========//","================================================");
+        Log.d("---","---");
+        try {
+            String filePath = dataPath + "/tessdata/" + lang + ".traineddata";
 
-    private void getTextFromImage(Bitmap bitmap){
-        TextRecognizer recognizer = new TextRecognizer.Builder(this).build();
-        if (recognizer.isOperational()){
-            Toast.makeText(GarbageCamera.this, "Error Occurred!!", Toast.LENGTH_SHORT);
-        }
-        else{
-            Frame frame = new Frame.Builder().setBitmap(bitmap).build();
-            SparseArray<TextBlock> textBlockSparseArray = recognizer.detect(frame);
-            StringBuilder stringBuilder = new StringBuilder();
-            for (int i=0; i < textBlockSparseArray.size(); i++){
-                TextBlock textBlock = textBlockSparseArray.valueAt(i);
-                stringBuilder.append(textBlock.getValue());
-                stringBuilder.append("\n");
+            AssetManager assetManager = getAssets();
+
+            InputStream inputStream = assetManager.open("tessdata/" + lang + ".traineddata");
+            OutputStream outputStream = new FileOutputStream(filePath);
+
+            byte[] buffer = new byte[1024];
+            int read;
+            while((read = inputStream.read(buffer)) != -1){
+                outputStream.write(buffer, 0, read);
             }
-            textview_data.setText(stringBuilder.toString());
-            button_capture.setText("Retake");
-            button_copy.setVisibility(View.VISIBLE);
+            outputStream.flush();
+            outputStream.close();
+            inputStream.close();
+        }
+        catch (Exception e){
+            e.printStackTrace();
         }
     }
-    private void copyToClipBoard(String text){
-        ClipboardManager clipBoard = (ClipboardManager) getSystemService(Context.CLIPBOARD_SERVICE);
-        ClipData clip = ClipData.newPlainText("Copied data", text);
-        clipBoard.setPrimaryClip(clip);
-        Toast.makeText(GarbageCamera.this, "Copied to clipboard", Toast.LENGTH_SHORT);
+
+
+    //TODO [로직 [7] : 카메라 및 갤러리에서 텍스트 사진가져와 이미지 뷰에 표시 실시]
+
+
+    //TODO [로직 [8] : 이미지 뷰에 표시된 사진을 OCR 컨버팅 수행 실시]
+    public void getOcrConvert(ImageView image){
+        Log.d("---","---");
+        Log.d("//===========//","================================================");
+        Log.d("","\n"+"[A_CameraOcr > getOcrConvert() 메소드 : OCR 인식 수행 실시]");
+        Log.d("//===========//","================================================");
+        Log.d("---","---");
+        try {
+            //TODO [drawable 리소스를 비트맵으로 가져오기]
+            //Bitmap icon = BitmapFactory.decodeResource(getResources(), R.drawable.icon_resource);
+
+            //TODO [이미지 뷰의 리소스를 비트맵으로 가져오기]
+            Bitmap bitmap = ((BitmapDrawable) image.getDrawable()).getBitmap();
+
+            //TODO [OCR 인식 수행]
+            tessBaseAPI.setImage(bitmap);
+            String result = String.valueOf(tessBaseAPI.getUTF8Text());
+
+            //TODO [Alert 팝업창 호출 실시]
+            getAlertDialog("[OCR] 사진 인식 결과",
+                    String.valueOf(result),
+                    "확인", "", "");
+
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+
+    //TODO [로직 [9] : 일반 Alert 팝업창 호출 메소드]
+    public void getAlertDialog(String header, String content, String ok, String no, String normal){
+
+        //TODO 타이틀 및 내용 표시
+        final String Tittle = header;
+        final String Message = content;
+
+        //TODO 버튼 이름 정의
+        String buttonNo = no;
+        String buttonYes = ok;
+        String buttonNature = normal;
+
+        //TODO AlertDialog 팝업창 생성
+        new AlertDialog.Builder(GarbageCamera.this)
+                .setTitle(Tittle) //[팝업창 타이틀 지정]
+                //.setIcon(R.drawable.tk_app_icon) //[팝업창 아이콘 지정]
+                .setMessage(Message) //[팝업창 내용 지정]
+                .setCancelable(false) //[외부 레이아웃 클릭시도 팝업창이 사라지지않게 설정]
+                .setPositiveButton(buttonYes, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        // TODO Auto-generated method stub
+                    }
+                })
+                .setNegativeButton(buttonNo, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        // TODO Auto-generated method stub
+                    }
+                })
+                .setNeutralButton(buttonNature, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        // TODO Auto-generated method stub
+                    }
+                })
+                .show();
     }
 }
--- app/src/main/java/com/example/reco/MainActivity.java
+++ app/src/main/java/com/example/reco/MainActivity.java
@@ -1,13 +1,28 @@
 package com.example.reco;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button GotoScan = findViewById(R.id.GotoScan);
+        GotoScan.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), GarbageCamera.class);
+                startActivity(intent);
+            }
+        });
     }
 
 }
